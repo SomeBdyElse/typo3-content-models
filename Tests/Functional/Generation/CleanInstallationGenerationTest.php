87f22055e906fad65520759e93c6150a81c9caa0
Hannes Lau <office@hanneslau.de>
2026-08-30 12:53:46 +0200

    [TASK] Add model generation burn tests
    * Test with TYPO3 v13 and v14
    * Test model generation with a clean TYPO3 install and with the
    styleguide extension.

---
+++ Tests/Functional/Generation/CleanInstallationGenerationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SomeBdyElse\Typo3ContentModels\Tests\Functional\Generation;
+
+use PHPUnit\Framework\Attributes\Test;
+
+final class CleanInstallationGenerationTest extends AbstractContentModelGenerationTestCase
+{
+    #[Test]
+    public function generatesContentModelsForCleanInstallation(): void
+    {
+        $generatedFiles = $this->generateContentModelsFromFixture('clean-content-models.yaml');
+
+        $this->assertGeneratedFileExists($generatedFiles, 'Pages/DefaultPage.php');
+        $this->assertGeneratedFileExists($generatedFiles, 'Content/TextContentModel.php');
+        $this->assertAtLeastOneGeneratedFileContains($generatedFiles, "#[ContentModel(table: 'pages', type: '1')]");
+        $this->assertAtLeastOneGeneratedFileContains($generatedFiles, "#[ContentModel(table: 'tt_content', type: 'text')]");
+    }
+}
---
+++ Tests/Functional/Generation/StyleguideInstallationGenerationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SomeBdyElse\Typo3ContentModels\Tests\Functional\Generation;
+
+use PHPUnit\Framework\Attributes\Test;
+
+final class StyleguideInstallationGenerationTest extends AbstractContentModelGenerationTestCase
+{
+    protected array $coreExtensionsToLoad = [
+        'felogin',
+        'fluid_styled_content',
+        'form',
+        'indexed_search',
+        'seo',
+    ];
+
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/content_models',
+        'typo3conf/ext/styleguide',
+    ];
+
+    #[Test]
+    public function generatesContentModelsForStyleguideInstallation(): void
+    {
+        $generatedFiles = $this->generateContentModelsFromFixture('styleguide-content-models.yaml');
+
+        $this->assertGeneratedFileExists($generatedFiles, 'Pages/DefaultPage.php');
+        $this->assertGeneratedFileExists($generatedFiles, 'Content/TextContentModel.php');
+        $this->assertAtLeastOneGeneratedFileContains($generatedFiles, "#[ContentModel(table: 'tx_styleguide_");
+    }
+}
